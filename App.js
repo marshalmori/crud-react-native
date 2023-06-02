80f6aa77e01051b01ee0f200daca25e17795c6ce
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-02 14:57:10 -0300

    Configurar a navegação para a tela de início

--- App.js
+++ App.js
@@ -2,9 +2,23 @@ import 'react-native-gesture-handler';
 import React from 'react';
 
 import {StyleSheet, Text, View} from 'react-native';
+import Inicio from './views/Inicio';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return <View></View>;
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Inicio">
+          <Stack.Screen name="Inicio" component={Inicio} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 };
 
 const styles = StyleSheet.create({});
