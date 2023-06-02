deba9c4a178f10344cb2642880ae6c7e5ec0f958
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-02 16:15:29 -0300

    Instalar o framework React Native Paper

--- App.js
+++ App.js
@@ -6,16 +6,38 @@ import Inicio from './views/Inicio';
 import NuevoCliente from './views/NuevoCliente';
 import DetallesCliente from './views/DetallesCliente';
 
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF',
+  },
+};
+
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Navigator
+          initialRouteName="Inicio"
+          screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: theme.colors.primary,
+            },
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
           <Stack.Screen name="Inicio" component={Inicio} />
           <Stack.Screen
             name="NuevoCliente"
