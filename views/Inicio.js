a75ee34c1bbd8522ce04b2b36a5a375779c64002
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-05 09:13:12 -0300

    Consultar API para obter os clientes

--- views/Inicio.js
+++ views/Inicio.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
+import axios from 'axios';
 
 const Inicio = () => {
+  // state de la app
+  const [clientes, guardarClientes] = useState([]);
+
+  useEffect(() => {
+    const obtenerClientesApi = async () => {
+      try {
+        const resultado = await axios.get('http://localhost:3000/clientes');
+        guardarClientes(resultado.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    obtenerClientesApi();
+  }, []);
   return <Text>Inicio</Text>;
 };
 
