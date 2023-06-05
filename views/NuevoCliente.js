e006a4cc82007fdbef3f57f8e2e9e5aa77042a80
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-04 21:01:12 -0300

    Adicionando os campos telefone, email e empresa no formulário de novo cliente

--- views/NuevoCliente.js
+++ views/NuevoCliente.js
@@ -4,12 +4,49 @@ import {TextInput, Headline, Button} from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
+  const leerNombre = () => {
+    console.log('Escrevendo...');
+  };
+
   return (
     <View style={globalStyles.contenedor}>
       <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
-      <TextInput />
+      <TextInput
+        label="Nombre"
+        placeholder="Digite seu nome"
+        onChange={() => leerNombre()}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Teléfono"
+        placeholder="Seu teléfono"
+        onChange={() => leerNombre()}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Correo"
+        placeholder="Seu correo"
+        onChange={() => leerNombre()}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Empresa"
+        placeholder="Nome da empresa"
+        onChange={() => leerNombre()}
+        style={styles.input}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+});
 
 export default NuevoCliente;
